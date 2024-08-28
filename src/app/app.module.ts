c6ecf77c57fd3820fe404ea2c3d1b164782cd879
AngelIpina12 <angelipina12@hotmail.com>
2024-08-27 22:19:34 -0600

    Creacion de componente dbz

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,19 @@ import { AppComponent } from './app.component';
 import { HeroComponent } from './heroes/hero/hero.component';
 import { ListComponent } from './heroes/list/list.component';
 import { CounterModule } from './counter/components/counter.module';
+import { DbzModule } from './dbz/dbz.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroComponent,
-    ListComponent
+    ListComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CounterModule
+    CounterModule,
+    DbzModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
